2f6d20d888301a4f48ce6737116312e1cc78c360
Wes Higbee <wes.mcclure@gmail.com>
2026-06-06 11:16:43 -0500

    node v24 ok

--- .config.fish
+++ .config.fish
@@ -1,3 +1,3 @@
-nvm use 22 # use v22 of node whenever I enter this repo
+nvm use 24 # v24 works if just tree-sitter-cli dep... if need tree-sitter too then v22 needed right now (on my mac)
 
 __local_config__pacakge_json_scripts_to_run_abbrs
